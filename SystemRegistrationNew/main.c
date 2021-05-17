34d15558db6e732b1a6f759e19f00253d7783111
Asku_SU <zaharandrei15@mail.ru>
2021-05-17 18:20:18 +0300

    Отладил и проверил на тестовых файлах

--- SystemRegistrationNew/main.c
+++ SystemRegistrationNew/main.c
@@ -12,39 +12,52 @@ typedef struct nameSpace
 
 } NameSpace;
 
-NameSpace* insertNameToNameSpace(NameSpace *previous, NameSpace *verifiable, short *exist)
-{
-    if (!previous) return verifiable;
-    short cmp = strcmp(verifiable->userName, previous->userName);
+NameSpace* insertNameToNameSpace(NameSpace *checkPos, NameSpace *verifiable, short *exist)
+{    
+    short cmp = strcmp(verifiable->userName, checkPos->userName);
     if (cmp == 0) 
     {
         *exist = 1;
-        return previous;
+        return checkPos;
     }
     else if (cmp < 0)
     {
-        if (!previous->leftName)
+        if (!checkPos->leftName)
         {
-            previous->leftName = verifiable;
+            checkPos->leftName = verifiable;
             return verifiable;
         }
         else
         {
-            return(insertNameToNameSpace(previous->leftName, verifiable, exist));
+            return(insertNameToNameSpace(checkPos->leftName, verifiable, exist));
         }
     }
     else if (cmp > 0)
     {
-        
+        if (!checkPos->rightName)
+        {
+            checkPos->rightName = verifiable;
+            return verifiable;
+        }
+        else
+        {
+            return(insertNameToNameSpace(checkPos->rightName, verifiable, exist));
+        }
     }
     
 }
 
-NameSpace higherName;
+NameSpace firstEmptyName;
 
 int main(){
     int N;
+    /*
+    FILE *inputF, *outputF;
     
+    inputF = fopen("input.txt", "r");
+    //outputF = fopen("output.txt", "w");
+    fscanf(inputF,"%d", &N);
+    */
     scanf("%d", &N);
     NameSpace *listNames;
     listNames = (NameSpace*)calloc(N, N * sizeof(NameSpace));
@@ -52,18 +65,20 @@ int main(){
     {
         NameSpace* currentName = listNames + i;
         scanf("%32s", currentName->userName);
+        //fscanf(inputF, "%32s", currentName->userName);        
         //fgets(strings + i*lengthS*sizeof(char), lengthS, stdin);
     }
     printf("\n");
 
     for (int i = 0; i < N; i++)
     {
-        short exist;
+        short exist = 0;
         NameSpace *currentName = listNames + i;
-        NameSpace *newElement = insertNameToNameSpace(&higherName, currentName, &exist);
-        if (exist == 0)
-                printf("OK\n");
-        else    printf("%s%d\n", newElement->userName, newElement->cntUsages);
+        NameSpace *element = insertNameToNameSpace(&firstEmptyName, currentName, &exist);
+        if (exist)        
+            printf("%s%d\n", element->userName, element->cntUsages);       
+        else printf("OK\n"); 
+        element->cntUsages++;  
     }
     return 0;
 }
