55a3536c9602830966a595c5ab1536a2dd149b5f
Asku_SU <zaharandrei15@mail.ru>
2021-05-17 23:49:03 +0300

    сделал ввод и начал делать сортировку

--- MysteriousPackage/main.c
+++ MysteriousPackage/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <malloc.h>
+
+typedef struct rectangle
+{
+    int w, h, index;
+} Rectangle;
+
+void quicksort(int *l, int *r){
+    if (r - l <= 1) return;
+    //int z = *(l + (r - 1));  
+}
 
 int main(){
+    int N;
+    Rectangle postcard;
+    scanf("%d%d%d", &N, &postcard.w, &postcard.h);
+    Rectangle *envelopes;
+    envelopes = (Rectangle*)calloc(N, sizeof(Rectangle));
+    for (int i = 0; i < N; i++)
+    {
+        Rectangle *currEnvel = envelopes + i;
+        scanf("%d%d", &currEnvel->w, &currEnvel->h);
+        currEnvel->index = i;
+    }
     
+
+    //printf("%d %d %d", N, envelopes->w, envelopes->h);
     return 0;
 }
